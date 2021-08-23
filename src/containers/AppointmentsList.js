16e67002cfe25e90c497859077f858370078dd68
El-Potato-Slayer <leighchad.cl@gmail.com>
2021-08-23 21:44:31 +0200

    component now fetches appointments of logged in user

--- src/containers/AppointmentsList.js
+++ src/containers/AppointmentsList.js
@@ -1,16 +1,42 @@
-function AppointmentsList() {
+import PropTypes from 'prop-types';
+import axios from 'axios';
+import { connect, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { setAppointments } from '../store/actions';
+
+function AppointmentsList({ appointments }) {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    axios.get('appointments', {
+      headers: {
+        Authorization: `token ${localStorage.getItem('token')}`,
+      },
+    }).then(
+      (res) => {
+        // console.log(res);
+        dispatch(setAppointments(res.data));
+      },
+    );
+  }, []);
+
   return (
-    <h1 data-testid="appointments">Appointments</h1>
+    <>
+      <h1 data-testid="appointments">Appointments</h1>
+      <div>
+        {appointments.map((appointment) => (
+          <p key={appointment.id}>{appointment.date}</p>
+        ))}
+      </div>
+    </>
   );
 }
 
-// const mapStateToProps = (state) => ({
-//   appointments: state.appointmentsReducer.appointments,
-// });
+const mapStateToProps = (state) => ({
+  appointments: state.appointmentsReducer.appointments,
+});
 
-// AgentsList.propTypes = {
-//   agents: PropTypes.instanceOf(Array).isRequired,
-// };
+AppointmentsList.propTypes = {
+  appointments: PropTypes.instanceOf(Array).isRequired,
+};
 
-// export default connect(mapStateToProps)(AgentsList);
-export default AppointmentsList;
+export default connect(mapStateToProps)(AppointmentsList);
