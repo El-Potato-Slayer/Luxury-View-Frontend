51eafa0e2dd2d693ff8ce91c4eb780fef908b0ce
El-Potato-Slayer <leighchad.cl@gmail.com>
2021-08-26 00:42:05 +0200

    added styling for AgentsListing component

--- src/containers/AgentsList.js
+++ src/containers/AgentsList.js
@@ -1,17 +1,47 @@
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 function AgentsList({ agents }) {
+  function displayAgent(agent) {
+    return (
+      <div>
+        <div
+          className="listing-image"
+          style={{ background: `url(${agent.picture}) center / cover` }}
+        />
+        <div className="info">
+          <p className="listing-important">
+            {agent.first_name}
+            {' '}
+            {agent.last_name}
+          </p>
+          <p>
+            Number:
+            {' '}
+            {agent.number}
+          </p>
+          <p>
+            Email:
+            {' '}
+            {agent.email}
+          </p>
+        </div>
+      </div>
+    );
+  }
+
   return (
-    <>
-      <h2 data-testid="agents">Agents</h2>
-      <div data-testid="agents-listing">
+    <div className="page">
+      <h2 data-testid="agents" className="page-title">Agents</h2>
+      <div data-testid="agents-listing" className="listings">
         {agents.map((agent) => (
-          <Link to={`/agents/${agent.id}`} key={agent.id}>{agent.first_name}</Link>
+          <div key={agent.id}>
+            {displayAgent(agent)}
+          </div>
         ))}
       </div>
-    </>
+    </div>
   );
 }
 
