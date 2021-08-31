4db3639ebe1250ce897a942438fd9ff58a186b31
El-Potato-Slayer <leighchad.cl@gmail.com>
2021-08-31 16:44:18 +0200

    added styling for Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,11 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 import { connect, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { setAuth, setUser } from '../store/actions';
 
 function Navbar({ isAuth }) {
+  const [burgerState, setBurgerState] = useState(false);
   const dispatch = useDispatch();
   const logout = () => {
     localStorage.clear();
@@ -11,30 +13,44 @@ function Navbar({ isAuth }) {
     dispatch(setUser({}));
   };
 
+  const handleBurgerState = () => {
+    setBurgerState(!burgerState);
+  };
+
   function authNavItem(isLoggedIn) {
-    // return isLoggedIn ? <Link data-testid="navbar-logout" to="/" onClick={logout}>Logout</Link>
-    //   : <Link data-testid="navbar-login" to="/login">Login</Link>;
     if (isLoggedIn) {
       return (
-        <Link data-testid="navbar-logout" to="/" onClick={logout}>Logout</Link>
+        <Link onClick={() => { logout(); handleBurgerState(); }} data-testid="navbar-logout" className="nav-item" to="/">Logout</Link>
       );
     }
     return (
       <>
-        <Link data-testid="navbar-login" to="/login">Login</Link>
-        <Link data-testid="navbar-register" to="/register">Register</Link>
+        <Link onClick={handleBurgerState} data-testid="navbar-login" className="nav-item" to="/login">Login</Link>
+        <Link onClick={handleBurgerState} data-testid="navbar-register" className="nav-item" to="/register">Register</Link>
       </>
     );
   }
 
   return (
-    <div data-testid="navbar" className="navbar">
-      <Link data-testid="navbar-homepage" to="/">Home</Link>
-      <Link data-testid="navbar-mansions" to="/mansions">Mansions</Link>
-      <Link data-testid="navbar-appointments" to="/appointments">Appointments</Link>
-      <Link data-testid="navbar-agents" to="/agents">Agents</Link>
-      {authNavItem(isAuth)}
-    </div>
+    <>
+      <div data-testid="navbar" className={`navbar${burgerState ? ' open' : ''}`}>
+        <div aria-hidden="true" onClick={handleBurgerState} onKeyDown={handleBurgerState} className={`dark-filter${!burgerState ? ' hide' : ''}`} />
+        <div className={`burger${burgerState ? ' open' : ''}`}>
+          <button type="button" id="burger-wrapper" className="burger-wrapper" onClick={handleBurgerState}>
+            <div id="burger-menu" className={`burger-menu${burgerState ? ' open' : ''}`} />
+          </button>
+        </div>
+        <img src="/assets/logo.png" className="nav-logo" alt="" />
+        <div className="nav-links">
+          <Link onClick={handleBurgerState} data-testid="navbar-homepage" className="nav-item" to="/">Home</Link>
+          <Link onClick={handleBurgerState} data-testid="navbar-mansions" className="nav-item" to="/mansions">Mansions</Link>
+          <Link onClick={handleBurgerState} data-testid="navbar-appointments" className="nav-item" to="/appointments">Appointments</Link>
+          <Link onClick={handleBurgerState} data-testid="navbar-agents" className="nav-item" to="/agents">Agents</Link>
+          {authNavItem(isAuth)}
+        </div>
+        <div className="nav-social" />
+      </div>
+    </>
   );
 }
 
