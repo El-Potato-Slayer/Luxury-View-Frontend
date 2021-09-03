ea2cafadc9968c3bfc8611fe656f76dff4e3cd62
El-Potato-Slayer <leighchad.cl@gmail.com>
2021-09-03 15:32:51 +0200

    fixed linter errors

--- src/tests/Home.test.js
+++ src/tests/Home.test.js
@@ -5,9 +5,9 @@ import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
 import '@testing-library/jest-dom';
+import { Provider } from 'react-redux';
 import Home from '../components/Home';
 import store from '../mocks/store/configureStore';
-import { Provider } from 'react-redux';
 import App from '../App';
 
 afterEach(cleanup);
@@ -15,13 +15,14 @@ afterEach(cleanup);
 test('Renders the Home component without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
-  <Provider store={store}>
-    <App>
-      <BrowserRouter>
-        <Home />
-      </BrowserRouter>
-    </App>
-  </Provider>, div);
+    <Provider store={store}>
+      <App>
+        <BrowserRouter>
+          <Home />
+        </BrowserRouter>
+      </App>
+    </Provider>, div,
+  );
 });
 
 test('Checks if title is rendered', () => {
@@ -48,7 +49,7 @@ describe('Newest Section', () => {
         </App>
       </Provider>,
     );
-    expect(screen.getByTestId('homepage-newest')).toHaveTextContent("Newest");
+    expect(screen.getByTestId('homepage-newest')).toHaveTextContent('Newest');
   });
 
   test('Checks that mansion names are rendered', () => {
@@ -61,10 +62,10 @@ describe('Newest Section', () => {
         </App>
       </Provider>,
     );
-    expect(screen.getByTestId('homepage-newest')).toHaveTextContent("Suganti Vapi");
-    expect(screen.getByTestId('homepage-newest')).toHaveTextContent("6 Bed Mansion");
-    expect(screen.getByTestId('homepage-newest')).toHaveTextContent("Videa Casale");
-    expect(screen.getByTestId('homepage-newest')).toHaveTextContent("Modern Trophy Estate");
+    expect(screen.getByTestId('homepage-newest')).toHaveTextContent('Suganti Vapi');
+    expect(screen.getByTestId('homepage-newest')).toHaveTextContent('6 Bed Mansion');
+    expect(screen.getByTestId('homepage-newest')).toHaveTextContent('Videa Casale');
+    expect(screen.getByTestId('homepage-newest')).toHaveTextContent('Modern Trophy Estate');
   });
 
   test('Checks that unknown mansion names are not rendered', () => {
@@ -77,10 +78,10 @@ describe('Newest Section', () => {
         </App>
       </Provider>,
     );
-    expect(screen.getByTestId('homepage-newest')).not.toHaveTextContent("Ooga");
-    expect(screen.getByTestId('homepage-newest')).not.toHaveTextContent("Booga");
+    expect(screen.getByTestId('homepage-newest')).not.toHaveTextContent('Ooga');
+    expect(screen.getByTestId('homepage-newest')).not.toHaveTextContent('Booga');
   });
-  
+
   test('Checks that mansion prices are rendered', () => {
     render(
       <Provider store={store}>
@@ -91,12 +92,12 @@ describe('Newest Section', () => {
         </App>
       </Provider>,
     );
-    expect(screen.getByTestId('homepage-newest')).toHaveTextContent("$35,000,000");
-    expect(screen.getByTestId('homepage-newest')).toHaveTextContent("$100,000,000");
-    expect(screen.getByTestId('homepage-newest')).toHaveTextContent("$26,000,000");
-    expect(screen.getByTestId('homepage-newest')).toHaveTextContent("$2,500,000");
+    expect(screen.getByTestId('homepage-newest')).toHaveTextContent('$35,000,000');
+    expect(screen.getByTestId('homepage-newest')).toHaveTextContent('$100,000,000');
+    expect(screen.getByTestId('homepage-newest')).toHaveTextContent('$26,000,000');
+    expect(screen.getByTestId('homepage-newest')).toHaveTextContent('$2,500,000');
   });
-  
+
   test('Checks that unknown mansion prices are not rendered', () => {
     render(
       <Provider store={store}>
@@ -107,12 +108,12 @@ describe('Newest Section', () => {
         </App>
       </Provider>,
     );
-    expect(screen.getByTestId('homepage-newest')).not.toHaveTextContent("$42");
-    expect(screen.getByTestId('homepage-newest')).not.toHaveTextContent("$1.78");
-    expect(screen.getByTestId('homepage-newest')).not.toHaveTextContent("$547");
-    expect(screen.getByTestId('homepage-newest')).not.toHaveTextContent("$425");
+    expect(screen.getByTestId('homepage-newest')).not.toHaveTextContent('$42');
+    expect(screen.getByTestId('homepage-newest')).not.toHaveTextContent('$1.78');
+    expect(screen.getByTestId('homepage-newest')).not.toHaveTextContent('$547');
+    expect(screen.getByTestId('homepage-newest')).not.toHaveTextContent('$425');
   });
-  
+
   test('Checks if mansion prices are not rendered without comma in Newest section', () => {
     render(
       <Provider store={store}>
@@ -123,12 +124,12 @@ describe('Newest Section', () => {
         </App>
       </Provider>,
     );
-    expect(screen.getByTestId('homepage-newest')).not.toHaveTextContent("$35000000");
-    expect(screen.getByTestId('homepage-newest')).not.toHaveTextContent("$100000000");
-    expect(screen.getByTestId('homepage-newest')).not.toHaveTextContent("$26000000");
-    expect(screen.getByTestId('homepage-newest')).not.toHaveTextContent("$2500000");
+    expect(screen.getByTestId('homepage-newest')).not.toHaveTextContent('$35000000');
+    expect(screen.getByTestId('homepage-newest')).not.toHaveTextContent('$100000000');
+    expect(screen.getByTestId('homepage-newest')).not.toHaveTextContent('$26000000');
+    expect(screen.getByTestId('homepage-newest')).not.toHaveTextContent('$2500000');
   });
-})
+});
 
 describe('Soon To Come section', () => {
   test('Checks if Soon To Come section is rendered', () => {
@@ -141,7 +142,7 @@ describe('Soon To Come section', () => {
         </App>
       </Provider>,
     );
-    expect(screen.getByTestId('homepage-soon')).toHaveTextContent("Soon");
+    expect(screen.getByTestId('homepage-soon')).toHaveTextContent('Soon');
   });
 
   test('Checks that card titles are rendered', () => {
@@ -154,10 +155,10 @@ describe('Soon To Come section', () => {
         </App>
       </Provider>,
     );
-    expect(screen.getByTestId('homepage-soon')).toHaveTextContent("Cars");
-    expect(screen.getByTestId('homepage-soon')).toHaveTextContent("Yachts");
-    expect(screen.getByTestId('homepage-soon')).toHaveTextContent("Jets");
-    expect(screen.getByTestId('homepage-soon')).toHaveTextContent("Helicopters");
+    expect(screen.getByTestId('homepage-soon')).toHaveTextContent('Cars');
+    expect(screen.getByTestId('homepage-soon')).toHaveTextContent('Yachts');
+    expect(screen.getByTestId('homepage-soon')).toHaveTextContent('Jets');
+    expect(screen.getByTestId('homepage-soon')).toHaveTextContent('Helicopters');
   });
 
   test('Checks that unknown card titles are not rendered', () => {
@@ -170,10 +171,10 @@ describe('Soon To Come section', () => {
         </App>
       </Provider>,
     );
-    expect(screen.getByTestId('homepage-soon')).not.toHaveTextContent("Ooga");
-    expect(screen.getByTestId('homepage-soon')).not.toHaveTextContent("Booga");
+    expect(screen.getByTestId('homepage-soon')).not.toHaveTextContent('Ooga');
+    expect(screen.getByTestId('homepage-soon')).not.toHaveTextContent('Booga');
   });
-})
+});
 
 describe('As Seen In section', () => {
   test('Checks if As Seen In section is rendered', () => {
@@ -186,7 +187,7 @@ describe('As Seen In section', () => {
         </App>
       </Provider>,
     );
-    expect(screen.getByTestId('homepage-seen')).toHaveTextContent("As seen in");
+    expect(screen.getByTestId('homepage-seen')).toHaveTextContent('As seen in');
   });
 
   test('Checks that images are rendered', () => {
@@ -206,17 +207,17 @@ describe('As Seen In section', () => {
     expect(screen.getByAltText('bbc-logo')).toBeInTheDocument();
     expect(screen.getByAltText('financial-times-logo')).toBeInTheDocument();
   });
-})
+});
 
 it('matches snapshot', () => {
   const tree = renderer.create(
     <Provider store={store}>
-    <App>
-      <BrowserRouter>
-        <Home />
-      </BrowserRouter>
-    </App>
-  </Provider>,
+      <App>
+        <BrowserRouter>
+          <Home />
+        </BrowserRouter>
+      </App>
+    </Provider>,
   ).toJSON();
   expect(tree).toMatchSnapshot();
 });
